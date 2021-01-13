bc3ef6d5f7158db21c3796e1b3d4d3bcc7649905
Carum <72158052+carum-carvi@users.noreply.github.com>
2021-01-13 23:05:11 +0100

    Add files via upload

---
+++ cow.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+
+
+clear;
+
+let a=0;
+let b=1;
+
+table=(
+"Tylko jedno w głowie mam" 
+"koksu 5 gram" 
+"odlecieć sam" 
+"W krainę zapomnienia" 
+"w głowie myśli mam" 
+"kiedy skończy się ten stan" 
+"gdy już nie będę sam" 
+"bo wjedzie biały węgorz" );
+
+while :
+do
+
+temp=$((b % 2))
+if [ $temp -eq 0 ]
+then
+echo "  ------ "
+echo " ${table[$a]} "
+echo "  ------ "
+echo "     \    ^__^ "
+echo "      \   (oo)\________ "
+echo "          (__)\        )\ /\ "
+echo "           U  ||-----w |"
+echo "              ||      || "
+let a=a+1
+sleep 2
+clear
+else
+echo "     ------ "
+echo "    ${table[$a]} "
+echo "     ------ "  
+echo "        \    ^__^ " 
+echo "         \   (oo)\________ " 
+echo "             (__)\        )\ /\ " 
+echo "              U  ||-----w |" 
+echo "                 ||      || "  
+let a=a+1 
+sleep 2 
+clear
+
+fi
+let b=b+1
+if [ $a -eq 8 ]
+then
+let a=0
+fi
+
+done
